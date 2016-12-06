e17bd9161047a9bb6b24fb17b06c889bec1286be
cruelpool <andrew.kieser@astontech.com>
2016-12-06 17:17:06 -0600

    546456456

--- src/com/company/NewClass.java
+++ src/com/company/NewClass.java
@@ -6,6 +6,6 @@ package com.company;
 public class NewClass {
     //dasdasdad
     public static void main(String[] args) {
-        System.out.println(6);
+        System.out.println(78);
     }
 }
